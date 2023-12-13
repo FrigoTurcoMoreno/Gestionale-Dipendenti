23b1861d08fec91df6504eb64254e04b58cf3dfe
Neha <ne.bhatti2@icloud.com>
2023-12-13 11:30:06 +0100

    modifica stile home

--- frontend/src/app/componenti/home/home.component.ts
+++ frontend/src/app/componenti/home/home.component.ts
@@ -1,41 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { LogsService } from 'src/app/servizi/logs.service';
+import { Component, OnInit } from "@angular/core";
+import { LogsService } from "src/app/servizi/logs.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
-export class HomeComponent implements OnInit{
-  constructor(private logService:LogsService){}
-  entrato?:boolean;
-  oreLavorate!:number;
+export class HomeComponent implements OnInit {
+  constructor(private logService: LogsService) {}
+  entrato?: boolean;
+  oreLavorate!: number;
   ngOnInit(): void {
     this.getLog();
-    this.logService.getOreMensili().subscribe((data)=>{
-      this.oreLavorate=data;
+    this.logService.getOreMensili().subscribe((data) => {
+      this.oreLavorate = data;
     });
   }
-  getLog(){
-    this.logService.getLogBE().subscribe((data)=>{
-      if(data){
-        this.entrato=true;
-      }else{
-        this.entrato=false;
+  getLog() {
+    this.logService.getLogBE().subscribe((data) => {
+      if (data) {
+        this.entrato = true;
+      } else {
+        this.entrato = false;
       }
-    })
+    });
   }
-  accedi(){
-    this.logService.entra().subscribe((data)=>{
-      this.entrato=true;
-    })
+  accedi() {
+    this.logService.entra().subscribe((data) => {
+      this.entrato = true;
+    });
   }
-  exit(){
-    this.logService.esci().subscribe((data)=>{
-      if(data){
+  exit() {
+    this.logService.esci().subscribe((data) => {
+      if (data) {
         this.getLog();
-        this.entrato=false;
+        this.entrato = false;
       }
-    })
+    });
   }
 }
