dfd620090214e2bcffae74f0c102bd8f894e8d10
FrigoTurcoMoreno <frigoturcomoreno@gmail.com>
2023-12-13 16:05:44 +0100

    riallineamento

--- backend/db.sql
+++ backend/db.sql
@@ -2,6 +2,6 @@ create database gestionale_dipendenti;
 
 use gestionale_dipendenti;
 
-insert into utenti(nome, cognome, email, password, is_admin, cancellato)
-values('Moreno', 'Frigo Turco', 'mft@gmail.com', '$2a$10$hktcDjcTsg/E0ompUQf6OOktWz352r9LN45NDUKcqPdURj7GE1Hiu', true, false);
+insert into utenti(nome, cognome, email, password, is_admin, abilitato)
+values('Moreno', 'Frigo Turco', 'mft@gmail.com', '$2a$10$hktcDjcTsg/E0ompUQf6OOktWz352r9LN45NDUKcqPdURj7GE1Hiu', true, true);
 -- la password dell'utente è 123456
